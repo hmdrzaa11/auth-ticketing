a4853fc7dbf1b3015b8cfbd8ba49078107ec6b3c
Hamid Oujand <hmdrzaa11@gmail.com>
2021-12-17 22:56:52 +0330

    added express-validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 let router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hi There!\n");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("password must between 4 and 20"),
+  ],
+  (req: Request, res: Response) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      //errors
+      return res.status(400).send(errors.array());
+    }
+    let { email, password } = req.body;
+    console.log("creating user ");
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
