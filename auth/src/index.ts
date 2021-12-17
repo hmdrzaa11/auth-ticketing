e66d168087d260dcde93d216e87e0f3e9165b61d
Hamid Oujand <hmdrzaa11@gmail.com>
2021-12-17 20:36:56 +0330

    added current-user route

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+
 let app = express();
 
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi There");
-});
+app.use(currentUserRouter);
 
 app.listen(3000, () => console.log("Auth listening on 3000"));
