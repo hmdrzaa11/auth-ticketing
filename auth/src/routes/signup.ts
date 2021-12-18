f653ba37d502d99dd489e8a36a2b2d4288d387ec
Hamid Oujand <hmdrzaa11@gmail.com>
2021-12-18 19:14:24 +0330

    added bad-request error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 import { User } from "../models/user";
 
 let router = express.Router();
@@ -14,11 +15,19 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("password must between 4 and 20"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
+    let { email, password } = req.body;
+    let existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestError("Email in use");
+    }
+    let user = User.build({ email, password });
+    await user.save();
+    res.status(201).send(user);
   }
 );
 
