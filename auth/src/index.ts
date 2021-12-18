eb57a64724628ecce67ff10acbb037e1c4214009
Hamid Oujand <hmdrzaa11@gmail.com>
2021-12-18 14:27:22 +0330

    added not-found error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 let app = express();
 
@@ -16,6 +17,9 @@ app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 
+app.all("*", () => {
+  throw new NotFoundError();
+});
 app.use(errorHandler);
 
 app.listen(3000, () => console.log("Auth listening on 3000"));
