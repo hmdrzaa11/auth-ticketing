d5648ed379570cf238c1ab68f0708fb58ebeac5a
Hamid Oujand <hmdrzaa11@gmail.com>
2021-12-18 15:43:07 +0330

    added mongoose connection to Mongo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { json } from "body-parser";
 import "express-async-errors";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -23,4 +24,14 @@ app.all("*", async () => {
 });
 app.use(errorHandler);
 
-app.listen(3000, () => console.log("Auth listening on 3000"));
+let start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+  app.listen(3000, () => console.log("Auth listening on 3000"));
+};
+
+start();
