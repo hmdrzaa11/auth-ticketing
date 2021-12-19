7ed8fa823587fe59bee0ed92a1915c4bb3ce93be
Hamid Oujand <hmdrzaa11@gmail.com>
2021-12-20 00:20:14 +0330

    refactor index.ts into app.ts file and export app instance

---
+++ auth/src/app.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+let app = express();
+
+app.set("trust proxy", true); //because traffic is going to be proxy from ingress-nginx into our express app
+//and by default express when sees any proxies not going to trust that connection
+
+app.use(json());
+app.use(cookieSession({ signed: false, secure: true }));
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
