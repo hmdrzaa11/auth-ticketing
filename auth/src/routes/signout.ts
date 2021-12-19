255312833d6f28f03955b11c5b0d4b9850ec17fb
Hamid Oujand <hmdrzaa11@gmail.com>
2021-12-19 20:15:33 +0330

    added signout route

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from "express";
 let router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("Hi There!\n");
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
