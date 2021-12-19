75fda2ce0ad86348617eae3bfe4b67d114a4ca25
Hamid Oujand <hmdrzaa11@gmail.com>
2021-12-19 20:10:17 +0330

    added current-user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 let router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi There!\n");
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    //decode it
+    let payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!); //if user some how mess the jwt here we get an error
+    //so we need to capture that err
+    res.send({ currentUser: payload });
+  } catch (err) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors/bad-request-error";
 import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { Password } from "../services/password";
 
 let router = express.Router();
 
@@ -11,7 +15,36 @@ router.post(
     body("password").trim().notEmpty().withMessage("password is required"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    let { email, password } = req.body;
+    let existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("invalid Email or Password");
+    }
+    let passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError("invalid Email or Password");
+    }
+    //generate token
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
